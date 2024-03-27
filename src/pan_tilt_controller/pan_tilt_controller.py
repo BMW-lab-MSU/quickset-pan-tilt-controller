b746536d3206292ae4d9ae8fb697537d074325b6
Trevor Vannoy <trevorvannoy@montana.edu>
2024-03-27 10:09:40 -0600

    follow pep8 style guide

--- src/pan_tilt_controller/pan_tilt_controller.py
+++ src/pan_tilt_controller/pan_tilt_controller.py
@@ -1,41 +1,44 @@
 import serial
 import time
 
+
 class PanTiltController:
     def __init__(self, ID):
-        self.ID: str = ID
-        self.fwVersion = None
-        self.softLimits = []
-        self.CommandList = set()
-    
+        self.id: str = ID
+        self.fw_version = None
+        self.soft_limits = []
+        self.CMD_LIST = set()
+
     def home(self):
         self.send(33, 0, 0)
-    
-    def moveDelta(self, pan, tilt):
+
+    def move_delta(self, pan, tilt):
         self.send(31, pan, tilt)
-    
-    def moveAbsolute(self,pan,tilt):
+
+    def move_absolute(self, pan, tilt):
         self.send(33, pan, tilt)
-        
-    def getStatus(self):
+
+    def get_status(self):
         Status = self.send(30)
         return Status
-    
-    def calcLRC(self, data):
+
+    def calculate_lrc(self, data):
         lrc = bytes.fromhex('00')  # Initialize LRC value to zero
         print(f"DATA: {data}")
         bytedata = [int(d) for d in data]
         print(f"INT DATA: {bytedata}")
         for byte in bytedata:
-            print(f"BYTE: {(byte)[2:].zfill(8)} for byte: {byte}")  # Print binary representation of each byte
+            # Print binary representation of each byte
+            print(f"BYTE: {(byte)[2:].zfill(8)} for byte: {byte}")
             lrc ^= byte
-        print(f"LRC: {bin(lrc)[2:].zfill(8)}")  # Print binary representation of the final LRC value
+        # Print binary representation of the final LRC value
+        print(f"LRC: {bin(lrc)[2:].zfill(8)}")
         return bytes([lrc])  # Return LRC value as a byte
 
     def send(self, cmd):
-        
-        self.serial.write(bytes.fromhex('02')) # Send Start
-        self.serial.write(bytes.fromhex(self.ID)) # Send ID
+
+        self.serial.write(bytes.fromhex('02'))  # Send Start
+        self.serial.write(bytes.fromhex(self.ID))  # Send ID
 
         if data is not None:
             command = bytes.fromhex(command)
