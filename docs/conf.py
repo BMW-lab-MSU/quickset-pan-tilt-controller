3231ea1a71556f4638b8c6a45d5e81c0ebf75264
Trevor Vannoy <trevorvannoy@montana.edu>
2025-05-05 13:26:18 -0600

    fix extension name

--- docs/conf.py
+++ docs/conf.py
@@ -17,7 +17,7 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
     'sphinx.ext.apidoc',
-    'sphinx.ext.napoelon'
+    'sphinx.ext.napoleon'
 ]
 
 templates_path = ['_templates']
